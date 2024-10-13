04135719bea90da239b0dbb41ca2ad5afe7b8236
alexfisher03 <fisherr.alexander@gmail.com>
2024-10-13 17:17:13 -0400

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/ACMWebPortfolio2/', //set this to your repository name with leading and trailing slashes
+});
